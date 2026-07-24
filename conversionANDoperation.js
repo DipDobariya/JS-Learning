08104da4cca1510d47fcbddbb25048c1e26ffe7a
DipDobariya <dipdobariya2085@gmail.com>
2026-07-24 20:08:33 +0530

    Add files via upload

---
+++ conversionANDoperation.js
@@ -0,0 +1,50 @@
+let score="28DT"  //String Value
+
+let valueInNumer=Number(score)   //Convert String Into Number
+
+// console.log(typeof score);
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);  //check value in actualy In Numer[Output:NaN=>Not A Number]
+
+//"28" =>  28
+//"28D" => NaN
+
+
+let isloggedIn = 4
+let BooleanLoggedIn = Boolean(isloggedIn)
+// console.log(typeof BooleanLoggedIn);
+// console.log(BooleanLoggedIn);
+
+
+
+// true=>1  / false=>0
+// "" => false
+// "Dip" => True
+
+
+//********* ********* Operations ********* *********
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+let str1="Hello"
+let str2=" Dip"
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1" + 2);   //12
+// console.log(1 + "2");   //12
+// console.log("1" + 2 + 2);   //12
+// console.log(1 + 2 + "2");   //32
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
