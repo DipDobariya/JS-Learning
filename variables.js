76ae28a47d808706a20d00dc710847150c68557a
DipDobariya <dipdobariya2085@gmail.com>
2026-07-24 19:17:51 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234 //fix value
+let accountEmail = "dip@gmail.com"
+var accountPass = "121212"
+accountCity = "Amreli"
+
+
+// accountId=22   Not Allowed
+accountEmail="dippp@gmail.com"
+accountPass="22222"
+accountCity="Gandhinagar"
+
+
+console.table([accountId,accountEmail,accountPass,accountCity])
+// console.log(accountId);
