8706d695d98f85809f26c9d9298af775662adfb3
DipDobariya <dipdobariya2085@gmail.com>
2026-07-24 19:32:10 +0530

    Add files via upload

---
+++ DataType.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all JS code as  newer version
+
+// alert(3+3)  we are using nodejs, not browser
+
+let name="Dip"  //String
+let age=21   //Integrt
+let weight=84.5   //float
+let islogin =true   //Boolean
+
+//Primitive DataTypes
+//number
+//BigInt
+//String
+//Boolean
+//null =>standalone value
+//undefined
+//symol =>unique
+
+
+//object
+console.log(typeof name);
