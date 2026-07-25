a3addbcef10c4d073710db8bc6f7bfb94f50054b
DipDobariya <dipdobariya2085@gmail.com>
2026-07-25 20:18:44 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,33 @@
+const myArray = [1,2,3,4,5,6]
+// console.log(myArray);
+// console.log(myArray[3]);
+
+
+const myarry = new Array(1,2,3,4)
+// console.log(myarry);
+
+//Array Method  
+
+// myarry.push(5)
+// myarry.pop(5)
+// myarry.unshift(0)
+// myarry.shift()
+
+// console.log(myarry.includes(3)); 
+
+// const newArry =myarry.join()
+
+// console.log(myarry);
+// console.log(newArry);
+
+console.log("A" ,myarry);
+
+const my1 =myarry.slice(1,3)
+
+console.log(my1);
+console.log("B", myarry);
+
+const my2 =myarry.splice(1,3)
+
+console.log("C", myarry);
+console.log(my2);
