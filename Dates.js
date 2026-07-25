60c62bc32769dfac6862523012981498ed856cf5
DipDobariya <dipdobariya2085@gmail.com>
2026-07-25 20:00:45 +0530

    Add files via upload

---
+++ Dates.js
@@ -0,0 +1,23 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+//  let newDate = new Date(2005,7,20)
+//  let newDate = new Date(2005,7,20,5,6,7)
+ let newDate = new Date("01-13-2005")
+//  console.log(newDate.toLocaleString());
+ 
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(newDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+ let datee=new Date()
+ console.log(datee);
+ console.log(datee.getDay());
+ console.log(datee.getMonth()+1);
+
+ 
