62953471482a0586dcd54ce97929557c72a6b7ec
DipDobariya <dipdobariya2085@gmail.com>
2026-07-26 11:47:10 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -31,3 +31,32 @@ const my2 =myarry.splice(1,3)
 
 console.log("C", myarry);
 console.log(my2);
+
+
+
+const marvel = ["Thor", "Spidy", "Ironman"]
+const dc = ["Superman", "flash", "batman"]
+
+// marvel.push(dc)
+// const all = marvel.concat(dc); 
+// console.log(marvel);
+// console.log(all);
+
+const all_heros = [...marvel,...dc]
+// console.log(all_heros);
+
+
+const another_arry = [1,2,3,[4,5,6],7,8,[9]]
+const real_Another_arry = another_arry.flat(Infinity)
+console.log(real_Another_arry);
+
+console.log(Array.isArray("Dip"));
+console.log(Array.from("Dip"));
+
+const score1 = 100
+const score2 = 200
+const score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+
