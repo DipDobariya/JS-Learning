b2203b947f4f537e88c338d2d1164b11e53b12c3
DipDobariya <dipdobariya2085@gmail.com>
2026-08-15 19:10:35 +0530

    Add files via upload

---
+++ Iteration.js
@@ -0,0 +1,82 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element == 5){
+    // console.log("5 is best number");
+    }
+    // console.log(element);
+    
+}
+
+
+for (let i = 0; i <=10; i++) {
+    console.log(`Outer loop ${i}`);
+    
+    for (let j = 0; j <=10; j++){
+    //console.log(`Inner Loop ${j} and  inner loop ${i} `);
+
+    // console.log(i + '*' + j + '=' + i*j);
+    
+    }
+}
+
+// let myArray = ['flash' , 'Spidy', ' batman']
+// console.log(myArray.length);
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     // console.log(element);
+    
+// }
+
+
+
+// break and continue
+
+// for (let index = 1; index <= 20 ; index++) {
+//     if(index == 5){
+//         console.log("Detected 5");
+//         break
+        
+//     }
+//    console.log(`value of i is ${index}`);    
+// }
+
+
+// for (let index = 1; index <= 20 ; index++) {
+//     if(index == 5){
+//         console.log("Detected 5");
+//         continue
+        
+//     }
+// //    console.log(`value of i is ${index}`);    
+// }
+
+
+//************************************* */
+
+//while  and do-while
+
+// let i = 0
+// while (i <=10) {
+//     console.log(`value of i is ${i}`);
+//     i = i +2
+// }
+
+
+// let myArray = ['flash' , 'Spidy', ' batman']
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value of ${myArray[arr]}`);
+//     arr = arr + 1
+    
+// }
+
+let scoree=1
+do {
+    console.log(`score is ${scoree}`);
+    // scoree =scoree +2
+    scoree++
+} while (scoree<=10);
