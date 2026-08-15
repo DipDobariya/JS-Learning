c680a92eed36421940b332f29f444b60cf3a4d43
DipDobariya <dipdobariya2085@gmail.com>
2026-08-15 12:24:56 +0530

    Add files via upload

---
+++ controlflow1.js
@@ -0,0 +1,48 @@
+//if  
+const isuser = true
+const temp = 40
+// if(temp < 50){
+//     console.log("less then 50");
+// }else{
+
+//     console.log("Temprature is Grater than 50");
+// }
+// console.log("Executed");
+
+
+
+
+// const score = 200
+// if (score >100){
+//     const power = "fly"
+//     console.log(`user Power : ${power}`);   
+// }
+
+
+// const balance = 1000
+// if(balance>500) console.log("test");
+
+// if(balance < 500){
+//     console.log("Less than");
+// }else if(balance < 750){
+//     console.log("Less Than 750");
+    
+// }else{
+//     console.log("Less Than 1200");
+    
+// }
+
+
+const user = true
+const debitcard = true
+const loginfromgoogle = false
+const loginfromgmail = true 
+
+if(user && debitcard && 2==2){
+    console.log("Allow to buy Course");
+}
+
+if(loginfromgoogle || loginfromgmail){
+    console.log("loggin success");
+    
+}
