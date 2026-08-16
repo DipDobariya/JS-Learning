f3a4b40dc0faa5ed3e759d5315dcf09e03932471
DipDobariya <dipdobariya2085@gmail.com>
2026-08-16 16:30:43 +0530

    Add files via upload

---
+++ forEach.js
@@ -0,0 +1,29 @@
+
+// const coding = ['js','py','rb']
+
+// const value = coding.forEach((item) => {
+//     console.log(item);
+//     return item
+    
+// })
+// console.log(value);
+
+
+// const num = [1,2,3,4,5]
+
+// const newNum = num.filter((no) => no > 4 )
+// const newNum = num.filter((no) => {
+//     return no > 4
+// } )
+
+const newNum = []
+num.forEach((no) => {
+    if(no>2){
+        newNum.push(no)
+    }
+})
+console.log(newNum);
+
+
+
+
